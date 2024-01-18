3303d4e2647d59f4b823cce62a46e8f4a650bed5
Armon <armon@nudgex.net>
2024-01-18 13:38:22 -0800

    removed settings.html: will just have speed as a function in the command

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 if __name__ == '__main__':
     app.run(debug=True)
