7d57280f63603db497da65598da207aaf38916bb
Armon <armon@nudgex.net>
2024-01-29 15:57:36 -0800

    switched from queue_condition to simple sleep loop for ease of testing

--- pump/connection.py
+++ pump/connection.py
@@ -1,4 +1,6 @@
 import serial
+import random
+import pdb
 import time
 import threading
 from collections import deque
@@ -12,14 +14,13 @@ baudrate=9600
 class Connection:
     """
     Manages the serial connection between server and instruments
-    multiple Pumps can depend on a single connection
+    multiple instruments can depend on a single connection
     """
-    def __init__(self, port,timeout=2):
+    def __init__(self, port,timeout=2.0):
 
         self.command_queue = deque() # basically a list but optomized to act as a queue
         self.response_queue = deque()
         self.queue_lock = threading.Lock()  # Lock to ensure thread-safe operations on queues
-        self.queue_condition = threading.Condition(self.queue_lock)  # Helps optimize wait times in process_queue()
         self.processing_thread = threading.Thread(target=self.process_queue)
         self.processing_thread.daemon = True  # Ensures thread exits when main program does, among other things
         self.processing_thread.start()
@@ -35,26 +36,29 @@ class Connection:
             self.command_queue.append(command_data)
 
     def get_response(self):
-        with self.queue_condition:
-            while not self.response_queue:
-                self.queue_condition.wait()
-            
-            with self.queue_lock:
-                response = self.response_queue.popleft()
+        while not self.response_queue:
+            time.sleep(0.1)
+        response = self.response_queue.popleft()
         return response
+
+    def log(self, message):
+        """
+        super simple debug log mostly used for unit tests
+        """
+        with open('connection_log.txt','a') as f:
+            f.write(message + '\n')
+        return 
     
     def process_queue(self):
         while True:
-            with self.queue_condition:
-                while not self.command_queue:
-                    self.queue_condition.wait()  # Wait for a signal that a command has been added
+            if self.command_queue:
                 command_data = self.command_queue.popleft()
-
-            self.send(command_data)
-            response = self.receive()
-
-            with self.queue_lock:  # Just need the lock here as we're not working with the condition
-                self.response_queue.append(response)
+                self.send(command_data)
+                response = self.receive()
+                with self.queue_lock:
+                    self.response_queue.append(response)
+            else:
+                time.sleep(0.1)
 
     def open(self, port):
         if self.connection.is_open:
@@ -82,7 +86,7 @@ class Connection:
 
     def terminal(self, timeout=5):
         """
-        For communicating directly with a pump
+        For communicating directly with the machine
         Great for troubleshooting but only used for dev and debug purposes
         """
         if self.connection:
